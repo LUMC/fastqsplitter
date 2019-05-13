0f0a64f762f57b46612114a87ffe0f83f76026c3
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-05-13 16:53:51 +0200

    improve

--- fastq_split.py
+++ fastq_split.py
@@ -6,7 +6,7 @@ import gzip
 
 from collections import namedtuple
 from pathlib import Path
-from typing import Optional, Iterable
+from typing import Optional, Iterable, List, Any
 from functools import reduce
 
 
@@ -17,6 +17,7 @@ class SimpleFastqParser(object):
     :arg handle: Any iterator returning lines of strings, preferable an open file handle
     :return SimpleFastqRecord objects
     """
+
     def __init__(self, handle):
         self.__handle = handle
         self.__bucket = [None, None, None, None]
@@ -49,7 +50,7 @@ def argument_parser() -> argparse.ArgumentParser:
 
 
 def fastq_reader(fastq_file: Path) -> Iterable:
-    with gzip.open(fastq_file, mode ='rt') as fastq_handle:
+    with gzip.open(fastq_file, mode='rt') as fastq_handle:
         fastq_parser = SimpleFastqParser(fastq_handle)
     return fastq_parser
 
@@ -57,8 +58,24 @@ def fastq_reader(fastq_file: Path) -> Iterable:
 def split_fastqs(input_file: Path, output_files: List[Path]):
     with gzip.open(str(input_file), mode='rt') as input_fastq:
         fastq_parser = SimpleFastqParser(input_fastq)
+    with contextlib.ExitStack as stack:
+        # Open all the files at once
+        # https://stackoverflow.com/questions/19412376/open-a-list-of-files-using-with-as-context-manager
+        output_handles = [
+            stack.enter_context(gzip.open(str(output_file), mode='wb'))
+            for output_file in output_files]  # type: List[gzip.GzipFile]
 
-
+    i = 0
+    number_of_output_files = len(output_handles)
+    while True:
+        # By using modulo we basically pick one file at a time.
+        file_to_write = output_handles[i % number_of_output_files]
+        for j in range(100):
+            try:
+                file_to_write.write(fastq_parser.next())
+            except StopIteration:
+                break
+        i += 1
 
 
 def main():
