4826b3e67d3a94a42283c54e671ee75426f79421
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-07-18 10:33:32 +0200

    simplify cython stuff

--- src/fastqsplitter/split.pyx
+++ src/fastqsplitter/split.pyx
@@ -20,56 +20,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import contextlib
-from pathlib import Path
+import io
 from typing import List
 
-# xopen opens files as normal files, gzip files, bzip2 files or xz files
-# depending on extension.
-import xopen
-
-# Choose 1 as default compression level. Speed is more important than filesize
-# in this application.
-DEFAULT_COMPRESSION_LEVEL = 1
-# There is no noticable difference in CPU time between 100 and 1000 group size.
-DEFAULT_GROUP_SIZE = 100
-# With one thread per file (pigz -p 1) pigz uses way less virtual memory
-# (10 vs 300 MB for 4 threads) and total CPU time is also decreased.
-# Example: 2.3 gb input file file. Five output files.
-# TT=Total cpu time. RT=realtime
-# Compression level 1: threads=1 RT=58s TT=3m33, threads=4 RT=57s TT=3m45
-# Compression level 5: threads=1 RT=1m48 TT=7m53, threads=4 RT=1m23, TT=8m39
-# But this is on a 8 thread machine. When using less threads RT will go towards
-# TT. Default compression is 1. So default threads 1 makes the most sense.
-DEFAULT_THREADS_PER_FILE = 1
-
-
-def split_fastqs(input_file: Path, output_files: List[Path],
-                 compression_level: int = DEFAULT_COMPRESSION_LEVEL,
-                 group_size: int = DEFAULT_GROUP_SIZE,
-                 threads_per_file: int = DEFAULT_THREADS_PER_FILE):
+def filesplitter(input_handle: io.BufferedReader,
+                 output_handles: List[io.BufferedWriter],
+                 lines_per_block = 100):
     cdef bytes line
-    cdef int groupsize = group_size * 4  # 4 lines per fastq record
+    cdef int blocksize = lines_per_block
     cdef int i
     cdef int group_no = -1
-    cdef int number_of_output_files = len(output_files)
-    # contextlib.Exitstack allows us to open multiple files at once which
-    # are automatically closed on error.
-    # https://stackoverflow.com/questions/19412376/open-a-list-of-files-using-with-as-context-manager
-    with contextlib.ExitStack() as stack:
-        input_fastq = stack.enter_context(
-            xopen.xopen(input_file, mode='rb'))  # type: xopen.PipedGzipReader
-        output_handles = [
-            stack.enter_context(xopen.xopen(
-                filename=output_file,
-                mode='wb',
-                compresslevel=compression_level,
-                threads=threads_per_file
-            )) for output_file in output_files
-        ]  # type: List[xopen.PipedGzipWriter]
+    cdef int number_of_output_files = len(output_handles)
 
-        for i, line in enumerate(input_fastq):
-            if i % groupsize == 0:
-                group_no += 1
-                write_to_output = output_handles[group_no % number_of_output_files].write
-            write_to_output(line)
+    for i, line in enumerate(input_handle):
+        if i % blocksize == 0:
+            group_no += 1
+            write_to_output = output_handles[
+                group_no % number_of_output_files].write
+        write_to_output(line)
