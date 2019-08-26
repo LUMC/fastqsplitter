fca495d1f44b59953331e03cc202ca1e4e52166c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-08-26 09:56:14 +0200

    more comments for clarifaction

--- src/fastqsplitter/split_py.py
+++ src/fastqsplitter/split_py.py
@@ -36,22 +36,32 @@ def filesplitter(input_handle: io.BufferedReader,
 
     group_number = 0
     number_of_output_files = len(output_handles)
+    # An input handle should be an iterable, but since this package uses xopen
+    # we cannot be sure.
     fastq_iterator = iter(input_handle)
+    # Alias the readline method for extra speed.
+    # next(handle) equals handle.readline() but throws a
+    # StopIteration error if the lines is ''.
     readline = fastq_iterator.__next__
     not_at_end_of_file = True
 
     while not_at_end_of_file:
         block = []
+        # This is the fastest way to read blocks of lines in pure python.
+        # The method used in split_cy is slower in pure python because it
+        # stores the counting integer as a PyObject.
         for j in range(lines_per_block):
             try:
-                # next(handle) equals handle.readline() but throws a
-                # StopIteration error if the lines is ''.
                 block.append(readline())
+            # Readline should throw a StopIteration at EOF
             except StopIteration:
                 not_at_end_of_file = False
                 break
 
+        # Select the output handle and write the block to it.
         output_handles[group_number].write(b"".join(block))
+
+        # Set the group number for the next group to be written.
         group_number += 1
         if group_number == number_of_output_files:
             group_number = 0
